e4eab215ba64f8320b44a39d0dfebcd65638fd09
Denchik136 <piterskijd36@gmail.com>
2024-05-24 22:44:27 +0300

    feed markup

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -1,9 +1,53 @@
 <template>
-  <div>Feed {{ apiUrl }}</div>
+  <div>
+    <div class="" v-if="isLoading">Loading</div>
+    <div class="" v-if="error">Something bad happened</div>
+
+    <div v-if="feed">
+      <div
+        class="articl-preview"
+        v-for="(article, index) in feed.articles"
+        :key="index"
+      >
+        <div class="article-meta">
+          <router-link
+            :to="{name: 'userProfile', params: {slug: article.author.username}}"
+          >
+            <img :src="article.author.image" />
+          </router-link>
+          <div class="info">
+            <router-link
+              :to="{
+                name: 'userProfile',
+                params: {slug: article.author.username},
+              }"
+              >{{ article.author.username }}
+            </router-link>
+            <span class="date">{{ article.createdAt }}</span>
+          </div>
+          <div class="pull-xs-right">ADD TO FAVORITES</div>
+        </div>
+        <router-link
+          :to="{name: 'article', params: {slug: article.slug}}"
+          class="preview-link"
+        >
+          <h1>{{ article.title }}</h1>
+          <p>{{ article.description }}</p>
+          <span>Read more...</span>
+          <ul class="tag-list">
+            TAG LIST
+          </ul>
+        </router-link>
+      </div>
+      PAGINATION
+    </div>
+  </div>
 </template>
 
 <script>
+import {mapState} from 'vuex'
 import {actionTypes} from '@/store/modules/feed'
+import feed from '@/api/feed'
 
 export default {
   name: 'McvFeed',
@@ -12,6 +56,14 @@ export default {
       type: String,
       required: true,
     },
+  },
+
+  computed: {
+    ...mapState({
+      feed: (state) => state.feed.data,
+      isLoading: (state) => state.feed.isLoading,
+      error: (state) => state.feed.errors,
+    }),
   },
   mounted() {
     console.log('init feed')
